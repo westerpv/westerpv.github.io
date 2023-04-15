caff41d153d7e1ae51d9932badbc35c9a1eb0e8c
westerpv <westerpv97@gmail.com>
2023-04-15 14:03:41 -0500

    version 3.0

--- html/js/slider-setting.js
+++ html/js/slider-setting.js
@@ -1,165 +1,37 @@
 /*---------------------------------------------------------------------
     File Name: slider-setting.js
 ---------------------------------------------------------------------*/
-
-"use strict";
-	var tpj = jQuery;
-
-	var revapi486;
-
-	const slider = document.querySelector('.slider');
-	const sliderWrapper = document.querySelector('.slider-wrapper');
-	const videos = [
-	  "images/fibra.mp4",
-	  'images/COSTELCOM.mp4',
-	  'images/conectate.mp4',
-	  'video4.mp4',
-	];
-	let currentVideoIndex = 0;
 	
-	function initSlider() {
-	  const video = document.createElement('video');
-	  video.src = videos[currentVideoIndex];
-	  video.autoplay = true;
-	  video.loop = true;
-	  video.muted = true;
-	  video.playbackRate = 0.5;
-	  slider.appendChild(video);
-	
-	  const prevBtn = document.createElement('button');
-	  prevBtn.classList.add('prev-btn');
-	  prevBtn.innerHTML = '&lt;';
-	  sliderWrapper.appendChild(prevBtn);
-	
-	  const nextBtn = document.createElement('button');
-	  nextBtn.classList.add('next-btn');
-	  nextBtn.innerHTML = '&gt;';
-	  sliderWrapper.appendChild(nextBtn);
-	
-	  prevBtn.addEventListener('click', () => {
-		changeVideo(-1);
+	const swiper = new Swiper('.swiper-container', {
+		direction: 'horizontal',
+		slidesPerView: 1,
+		loop: true,
+		navigation: {
+		  prevEl: '.swiper-button-prev',
+		  nextEl: '.swiper-button-next',
+		},
+		autoplay: {
+			delay: 5000, // tiempo en milisegundos entre cada transición de diapositiva
+			disableOnInteraction: false, // para que el autoplay no se detenga al interactuar con el slider
+		  },
+		  pauseOnMouseEnter: true, // para que el autoplay se detenga al pasar el mouse sobre el slider
+		  navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		  },
+		on: {
+		  slideChange: function() {
+			const activeVideo = this.slides[this.activeIndex].querySelector('video');
+			activeVideo.play();
+		  },
+		  slideNextTransitionStart: function() {
+			const prevVideo = this.slides[this.previousIndex].querySelector('video');
+			prevVideo.pause();
+		  },
+		  slidePrevTransitionStart: function() {
+			const prevVideo = this.slides[this.previousIndex].querySelector('video');
+			prevVideo.pause();
+		  },
+		},
 	  });
-	
-	  nextBtn.addEventListener('click', () => {
-		changeVideo(1);
-	  });
-	}
-	
-	function changeVideo(direction) {
-	  currentVideoIndex += direction;
-	
-	  if (currentVideoIndex < 0) {
-		currentVideoIndex = videos.length - 1;
-	  } else if (currentVideoIndex >= videos.length) {
-		currentVideoIndex = 0;
-	  }
-	
-	  const video = slider.querySelector('video');
-	  video.src = videos[currentVideoIndex];
-	  video.load();
-	  video.play();
-	}
-	
-
-
-
-	initSlider();
-	
-
-	tpj(document).ready(function () {
-		if (tpj("#rev_slider_486_1").revolution == undefined) {
-			revslider_showDoubleJqueryError("#rev_slider_486_1");
-		} else {
-			revapi486 = tpj("#rev_slider_486_1").show().revolution({
-				sliderType: "standard",
-				jsFileLocation: "revolution/js/",
-				sliderLayout: "fullscreen",
-				dottedOverlay: "none",
-				delay: 5000,
-				navigation: {
-					keyboardNavigation: "on",
-					keyboard_direction: "horizontal",
-					mouseScrollNavigation: "off",
-					mouseScrollReverse: "default",
-					onHoverStop: "on",
-					touch: {
-						touchenabled: "on",
-						swipe_threshold: 75,
-						swipe_min_touches: 1,
-						swipe_direction: "horizontal",
-						drag_block_vertical: false
-					},
-					arrows: {
-						  style: "gyges",
-						  enable: true,
-						  hide_onmobile: false,
-						  hide_onleave: true,
-						  hide_delay: 200,
-						  hide_delay_mobile: 1200,
-						  tmp: '',
-						  left: {
-							  h_align: "left",
-							  v_align: "center",
-							  h_offset: 0,
-							  v_offset: 0
-						  },
-						  right: {
-							  h_align: "right",
-							  v_align: "center",
-							  h_offset: 0,
-							  v_offset: 0
-						  }
-					  },
-					bullets: {
-						enable: true,
-						hide_onmobile: true,
-						hide_under: 800,
-						style: "hebe",
-						hide_onleave: false,
-						direction: "horizontal",
-						h_align: "center",
-						v_align: "bottom",
-						h_offset: 0,
-						v_offset: 30,
-						space: 5,
-						tmp: '<span class="tp-bullet-image"></span><span class="tp-bullet-imageoverlay"></span><span class="tp-bullet-title"></span>'
-					}
-				},
-				viewPort: {
-					enable: true,
-					outof: "pause",
-					visible_area: "70%",
-					presize: false
-				},
-				responsiveLevels: [1240, 1024, 778, 480],
-				visibilityLevels: [1240, 1024, 778, 480],
-				gridwidth: [1240, 1024, 778, 480],
-				gridheight: [500, 450, 400, 350],
-				lazyType: "none",
-				parallax: {
-					type: "scroll",
-					origo: "enterpoint",
-					speed: 400,
-					levels: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 46, 47, 48, 49, 50, 55],
-					type: "scroll",
-				},
-				shadow: 0,
-				spinner: "off",
-				stopLoop: "off",
-				stopAfterLoops: -1,
-				stopAtSlide: -1,
-				shuffle: "off",
-				autoHeight: "off",
-				hideThumbsOnMobile: "off",
-				hideSliderAtLimit: 0,
-				hideCaptionAtLimit: 0,
-				hideAllCaptionAtLilmit: 0,
-				debugMode: false,
-				fallbacks: {
-					simplifyAll: "off",
-					nextSlideOnWindowFocus: "off",
-					disableFocusListener: false,
-				}
-			});
-		}
-	});
+	  
